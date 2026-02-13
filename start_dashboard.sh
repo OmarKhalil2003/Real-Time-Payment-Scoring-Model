8fed6918fb38b55b7c1c1fb9fb4c16dc098fd94b
Omar Khalil <96587367+OmarKhalil2003@users.noreply.github.com>
2026-02-13 02:19:15 +0200

    Update start_dashboard.sh

--- start_dashboard.sh
+++ start_dashboard.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo ""
 echo "============================================================"
 echo "🚀 STREAMLIT DASHBOARD IS STARTING..."
 echo "👉 Open your browser at: http://localhost:8501"
 echo "============================================================"
-echo ""
 
-streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0
+streamlit run dashboard.py \
+  --server.port=8501 \
+  --server.address=0.0.0.0
